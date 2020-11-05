0700a827d9294d0fe46783a8da5b56c9f16b83f4
Rachelami <rachelami@gmail.com>
2020-11-05 21:31:46 +0200

    interval to update

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Chart } from "react-google-charts";
 
 const Map = ({ geoMapData }) => {
+    const [updateMap, setUpdateMap] = useState(0)
+
+    useEffect(() => {
+        setTimeout(() => setUpdateMap(updateMap + 1), 60000);
+    }, [updateMap]);
 
     return (
         <div className="map-border">
