78db23bf37ecdc0f440f510f05f643f0e608d52a
Rachelami <rachelami@gmail.com>
2020-11-05 19:45:30 +0200

    hamburger navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+    const [isHide, setIsHide] = useState(false)
+
+    const handleHamburgerClick = () => {
+        setIsHide(!isHide)
+    }
+
+
     return (
         <div className="navbar-container">
-            <div className="nav-link-box">
-                <img src={process.env.PUBLIC_URL + '/images/location.svg'} className="nav-logos" />
-                <Link to={'/'} className="link"> Map </Link>
-            </div>
-            <div className="nav-link-box">
-                <img src={process.env.PUBLIC_URL + '/images/search.svg'} className="nav-logos" />
-                <Link to={'/page'} className="link"> Add Users </Link>
+
+            <img src={process.env.PUBLIC_URL + `${isHide ? '/images/cancel.png' : '/images/hamburger.png'}`} className={isHide ? "cancel-logo nav-hamburger-container": 'hamburger-logo nav-hamburger-container'} onClick={() => handleHamburgerClick()} />
+
+            <div className={isHide ? 'show-navbar navbar-expand' : 'navbar-expand'}>
+                <div className="nav-link-box">
+                    <img src={process.env.PUBLIC_URL + '/images/location.svg'} className="nav-logos" />
+                    <Link to={'/'} className="link"> Map </Link>
+                </div>
+                <div className="nav-link-box">
+                    <img src={process.env.PUBLIC_URL + '/images/search.svg'} className="nav-logos" />
+                    <Link to={'/page'} className="link"> Add Users </Link>
+                </div>
             </div>
         </div>
     );
